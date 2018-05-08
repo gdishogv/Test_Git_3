531aa729c82d5fc8ec56c882710ad7616be01c0d
namlxhoya <namlx.it@md.hoya.co.jp>
2018-05-08 16:29:14 +0700

    update the second

--- Test_Git_3/Form1.vb
+++ Test_Git_3/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click
         MessageBox.Show("data")
     End Sub
 End Class
